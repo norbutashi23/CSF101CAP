cadf6be442c13f277d3854117eef0d503657ee69
norbutashi23 <02230108.cst@rub.edu.bt>
2024-04-11 22:51:31 +0530

    second commit

--- CAP1_02230108.py
+++ CAP1_02230108.py
@@ -4,40 +4,41 @@
 # 02230108
 #######################################
 # Reference 
+# W3 School
+# https://www.programiz.com/
  
  
 #######################################
 # Solution
-# Your Solution Score:
-# Put your number here
+# Solution Score: 49960
 #######################################
 
-def read_input_file(file_path):
-    """
-    Reads the input file, the function coverts its content into a list of pairs. Two pieces of information are contained in each pair:
-    the player's chosen shape and the round's outcome. The information from ever game round is contained in this pairwise collection.
-    """
+# Funstion to read game results from a file
+def read_input(file_path):
+    # Initialize an empty list to store game results
     game_results = []
+    # Open the file specified by file_path in read mode
     with open(file_path, 'r') as file:
+        # Iterate over each line in the file
         for line in file:
             shape,outcome = line.strip().split()
-            game_results.append((shape, outcome))
+            game_results.append((shape, outcome))      # Append the tuple (shape, outcome) to game_result
     return game_results
 
 def calculate_score(game_results):
     """
     Calculates the total score based on round results.
     """
-    shape_score = {'A': 1, 'B': 2, 'C': 3}  # Mapping of shape to score
-    outcome_scores = {'X': 0, 'Y': 3, 'Z': 6} # Mapping of outcome to score
+    shape_score = {'A': 1, 'B': 2, 'C': 3}  # Dictionary mapping each shape to  its respective score
+    outcome_scores = {'X': 0, 'Y': 3, 'Z': 6} # Dictionary mapping each outcome to its respective score
     
-    score = sum(shape_score[shape] + outcome_scores[outcome] for shape, outcome in game_results)
+    score = sum(shape_score[shape] + outcome_scores[outcome] for shape, outcome in game_results)   # Calculates the total score by summing up the scores for each round
     return score
 
-# Example usage:
-file_path = "input_8_cap1.txt" 
-results = read_input_file(file_path)
-total_score = calculate_score(results)
+
+file_path = "input_8_cap1.txt"      # Specify the path to the input file
+results = read_input(file_path)        # Read the game_results from the file
+total_score = calculate_score(results)     # Calculates the total score based on the game_results
 print("Total Score:", total_score)
 
 
