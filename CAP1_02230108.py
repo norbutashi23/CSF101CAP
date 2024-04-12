3efa19d167433438855fa554578fbea70432a9df
norbutashi23 <02230108.cst@rub.edu.bt>
2024-04-13 02:24:42 +0530

    first commit

--- CAP1_02230108.py
+++ CAP1_02230108.py
@@ -7,37 +7,64 @@
 # https://www.w3schools.com/
 # https://www.programiz.com/
 # https://youtube.com/playlist?list=PLZPZq0r_RZOOkUQbat8LyQii36cJf2SWT&si=hqH5NEnCcYRi5nYx
+# https://youtu.be/GqBoZZ6ZJxk
 
  
  
 #######################################
-# Solution Score: 49960
+# Solution Score: 51010
 #######################################
 
-# Funstion to read game results from a file
+# Function to read game outcome from a file
 def read_input(file_path):
-    # Initialize an empty list to store game results
-    game_results = []
+    # Initialize an empty list to store game outcome
+    game_outcomes = []
     # Open the file specified by file_path in read mode
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r') as r:
         # Iterate over each line in the file
-        for line in file:
-            shape,outcome = line.strip().split()
-            game_results.append((shape, outcome))      # Append the tuple (shape, outcome) to game_result
-    return game_results
+        for line in r:
+            figure,result = line.strip().split()
+            
+            # Append the tuple (figure, outcome) to game round
+            game_outcomes.append((figure, result))
+    return game_outcomes
 
-def calculate_score(game_results):
-    """
-    Calculates the total score based on round results.
-    """
-    shape_score = {'A': 1, 'B': 2, 'C': 3}  # Dictionary mapping each shape to  its respective score
-    outcome_scores = {'X': 0, 'Y': 3, 'Z': 6} # Dictionary mapping each outcome to its respective score
-    
-    score = sum(shape_score[shape] + outcome_scores[outcome] for shape, outcome in game_results)   # Calculates the total score by summing up the scores for each round
+
+
+# Calculate the total score based on the game outcome
+def calculate_score(game_outcomes):
+    score = 0
+    # Loop through each game outcome
+    for figure, result in game_outcomes:
+        # Assigns scores based on the figure(shape) and the result(outcome)
+        if figure == "A":
+            if result == "X":
+                figure_score = 3 # Score for Figure A with result X
+            elif result == "Y":
+                figure_score == 4 # Score for figure A with result B
+            else:
+                figure_score = 8  # Score for figure A with any other result
+        elif figure == "B":
+            if result == "X":
+                figure_score = 1 # Score for Figure B with result X
+            elif result == "Y":
+                figure_score = 5 # Score for Figure B with result Y
+            else:
+                figure_score = 9 # Score for figure B with any other result
+        else:
+            if result == "X":
+                figure_score = 2 # Score for any Figure with result X
+            elif result == "Y":
+                figure_score = 6 # Score for any Figure with result Y
+            else:
+                figure_score = 7 # Score for any Figure with any other result
+        score += figure_score  # Gives total score
     return score
 
+# Specify the file path for the input data
+file_path = "input_8_cap1.txt" 
 
-file_path = "input_8_cap1.txt"      # Specify the path to the input file
-results = read_input(file_path)        # Read the game_results from the file
-total_score = calculate_score(results)     # Calculates the total score based on the game_results
+# Read game outcomes from the specified file
+game_outcomes = read_input(file_path)
+total_score = calculate_score(game_outcomes)
 print("Total Score:", total_score)
